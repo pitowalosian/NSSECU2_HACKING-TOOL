0357e7cfea26305fa07304b0bba65013c9a2c0df
Trisha <trisha_bendol@dlsu.edu.ph>
2026-08-09 11:19:42 +0800

    added sensitive filename scanner and results display

--- scanner.py
+++ scanner.py
@@ -27,30 +27,83 @@ def load_wordlist(filepath):
 
     return wordlist
 
-def is_sensitive(filename):
-    """Return true if filename is part of wordlist"""
+def is_sensitive(filename, ext_list):
+    """Return True if filename has a sensitive extension."""
 
     name_lower = filename.lower()
-    ext_list = load_wordlist("wordlists/extensions_list.txt")
 
     for ext in ext_list:
         if name_lower.endswith(ext):
             return True
 
-    # TODO I think we can also scan sensitive filenames?
+    return False
+ 
+def is_sensitive_filename(filename, sensitive_list):
+    """Return true if filename contains a sensitive keyword."""
 
-def scan(path):
+    name_lower = filename.lower()
+
+    for sensitive in sensitive_list:
+        if sensitive in name_lower:
+            return True
+
+    return False
+        
+def scan(path, ext_list, filename_list):
     
     sensitive_files = []
 
     for (dirpath, dirnames, filenames) in walk(path):
         for f in filenames:
             fullpath = join(dirpath, f)
-            if isfile(fullpath) and is_sensitive(f):
-                sensitive_files.append(fullpath)
+            if is_sensitive(f, ext_list):
+                sensitive_files.append((fullpath, "Sensitive extension"))
+
+            elif is_sensitive_filename(f, filename_list):
+                sensitive_files.append((fullpath, "Sensitive filename"))
 
     return sensitive_files
 
+def display_results(path, findings):
+    # For findings
+    extension_count = 0
+    filename_count = 0
+
+    for file, reason in findings:
+        if reason == "Sensitive extension":
+            extension_count += 1
+        elif reason == "Sensitive filename":
+            filename_count += 1
+
+    # Results
+    print("=====================================================================")
+    print("                              SCAN RESULTS")
+    print("=====================================================================")
+
+    print(f"\nTarget:")
+    print(f"{args.path}")
+
+    print(f"\nTotal findings: {len(findings)}")
+    print(f"Sensitive extensions: {extension_count}")
+    print(f"Sensitive filenames: {filename_count}")
+
+    print("\n=====================================================================")
+    print("SENSITIVE FILES")
+    print("=====================================================================")
+
+    if findings:
+        for i, (file, reason) in enumerate(findings, start=1):
+            filename = os.path.basename(file)
+
+            print(f"\n[{i}] {reason}")
+            print(f"    File: {filename}")
+            print(f"    Path: {file}")
+    else:
+        print("\nNo sensitive files found.")
+
+    print("\n" + "=====================================================================")
+    print("                      ~~!SCAN COMPLETE!~~")
+    print("=====================================================================")
 
 if __name__ == "__main__":
     # to run: python scanner.py --path [insert path]
@@ -63,12 +116,12 @@ if __name__ == "__main__":
 
     print(f"Scanning {args.path} for sensitive files.\n\n")
 
-    f = scan(args.path)
+    # Loading the wordlists
+    ext_list = load_wordlist("wordlists/extensions_list.txt")
+    filename_list = load_wordlist("wordlists/sensitive_filenames.txt")
 
-    if f:
-        print(f"Found {len(f)} potentially sensitive files: \n")
-        
-        for file in f:
-            print(f" - {file}")
-    else:
-        print("No sensitive files found.")
+    # Scanning
+    findings = scan(args.path, ext_list, filename_list)
+
+    # Display the results
+    display_results(args.path, findings)
